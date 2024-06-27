4981b214056bc5c3bf3a05edd236aa671c0ca9e2
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-06-27 16:52:02 -0400

    Generate computer choice

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,1 +1,34 @@
-console.log("Hello World");
+// Rock Paper Scissors - Game will be played entirely in the console
+
+function getComputerChoice() {
+    /*
+    Randomly choose value between 1 - 3
+    IF random value is equal to 1 THEN
+        RETURN "rock"
+    ELSE IF random value is equal to 2 THEN
+        RETURN "paper"
+    ELSE IF random value is equal to 3 THEN
+        RETURN "scissors"
+    ELSE
+        RETURN "invalid number generated"
+    ENDIF
+    */
+
+    let computerChoice = generateChoice()
+    if (computerChoice === 1) {
+        return "rock"
+    } else if (computerChoice === 2) {
+        return "paper"
+    } else if (computerChoice === 3) {
+        return "scissors"
+    } else {
+        return "invalid number generated"
+    }
+}
+
+function generateChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    return choice;
+}
+
+console.log(getComputerChoice());
