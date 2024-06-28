37e98b29106165b2be681a2ce6f600b3c52722be
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-06-28 17:25:48 -0400

    Create playRound function and add missing semi-colons throughout code

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -20,13 +20,13 @@ function getComputerChoice() {
 
     let computerChoice = generateChoice()
     if (computerChoice === 1) {
-        return "rock"
+        return "rock";
     } else if (computerChoice === 2) {
-        return "paper"
+        return "paper";
     } else if (computerChoice === 3) {
-        return "scissors"
+        return "scissors";
     } else {
-        return "invalid number generated"
+        return "invalid number generated";
     }
 }
 
@@ -38,33 +38,78 @@ function generateChoice() {
 function getHumanChoice() {
     /*
     READ user input (r = rock, p = paper, s = scissors)
-    IF input is equal to p:
+    IF input is equal to p THEN
         RETURN "paper"
-    ELSE IF input is equal to r:
+    ELSE IF input is equal to r THEN
         RETURN "rock"
-    ELSE IF input is equal to s:
+    ELSE IF input is equal to s THEN
         RETURN "scissors"
-    ELSE IF input is equal to null:
+    ELSE IF input is equal to null THEN
         RETURN "no input given"
     ELSE:
         RETURN "invalid input"
+    ENDIF
     */
 
     let humanChoice = prompt("Input your choice (r = rock, p = paper, s = scissors): ")
     if (humanChoice == "r") {
-        return "rock"
+        return "rock";
     } else if (humanChoice == "p") {
-        return "paper"
+        return "paper";
     } else if (humanChoice == "s") {
-        return "scissors"
-
+        return "scissors";
     } else if (humanChoice == null) {
-        return "no input given"
+        return "no input given";
     } else {
-        return "invalid input"
+        return "invalid input";
     }
 
 }
 
-console.log("Computer Choice: " + getComputerChoice());
-console.log("User Choice: " + getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    /*
+    SET humanChoice and computerChoice variables to lower case
+    IF humanChoice is equal to computerChoice THEN
+        DISPLAY tie message
+    ELSE IF (humanChoice is rock AND computerChoice is scissors) OR 
+            (humanChoice is paper AND computerChoice is rock) OR 
+            (humanChoice is scissors AND computerChoice is paper) THEN
+        INCREMENT human choice score
+        DISPLAY human winner message
+    ELSE IF (computerChoice is rock AND humanChoice is scissors) OR 
+            (computerChoice is paper AND humanChoice is rock) OR 
+            (computerChoice is scissors AND humanChoice is paper) THEN
+        INCREMENT computer choice score
+        DISPLAY computer winner message
+    ELSE 
+        DISPLAY invalid round message
+    ENDIF
+    */
+   let hC = humanChoice.toLowerCase();
+   let cC = computerChoice.toLowerCase();
+
+   if (hC === cC) {
+        console.log("Tie Game!");
+
+   } else if ((hC === "rock" && cC === "scissors") || 
+              (hC === "paper" && cC === "rock") || 
+              (hC === "scissors" && cC === "paper")) {
+        humanScore++;
+        console.log(`You win! ${hC} beats ${cC}.`);
+
+   } else if ((cC === "rock" && hC === "scissors") || 
+              (cC === "paper" && hC === "rock") || 
+              (cC === "scissors" && hC === "paper")) {
+        computerScore++;
+        console.log(`You lose! ${cC} beats ${hC}.`);   
+    } else {
+        console.log("Try again. You have inputted in an invalid choice.");
+   }
+}
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+console.log(humanSelection, computerSelection);
+
+playRound(humanSelection, computerSelection);
