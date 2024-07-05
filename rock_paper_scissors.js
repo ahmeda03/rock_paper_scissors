40b24a93053be4e2fb2bbbfb83db7b10ae65eb0f
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-07-05 19:22:21 -0400

    Create playGame func and edit playRound func

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,9 +1,5 @@
 // Rock Paper Scissors - Game will be played entirely in the console
 
-// Global Variables
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     /*
     Randomly choose value between 1 - 3
@@ -66,50 +62,118 @@ function getHumanChoice() {
 
 }
 
-function playRound(humanChoice, computerChoice) {
+function playGame() {
     /*
-    SET humanChoice and computerChoice variables to lower case
-    IF humanChoice is equal to computerChoice THEN
+    INIT playerScore, computerScore to 0
+    INIT playerChoice, computerChoice to NULL
+
+    FOR each round (5 rounds)
+        SET playerChoice by CALLING getHumanChoice
+        SET computerChoice by CALLING getComputerChoice
+        CALL playRound with playChoice and computerChoice RETURNING winnerString
+
+        IF winnerString is player THEN
+            INCREMENT playScore
+        ELSE IF winnerString is computer THEN
+            INCREMENT computerScore
+        ELSE
+            DISPLAY invalid winner message
+        ENDIF
+    ENDFOR
+
+    IF (playerScore == computerScore) THEN
         DISPLAY tie message
-    ELSE IF (humanChoice is rock AND computerChoice is scissors) OR 
-            (humanChoice is paper AND computerChoice is rock) OR 
-            (humanChoice is scissors AND computerChoice is paper) THEN
-        INCREMENT human choice score
-        DISPLAY human winner message
-    ELSE IF (computerChoice is rock AND humanChoice is scissors) OR 
-            (computerChoice is paper AND humanChoice is rock) OR 
-            (computerChoice is scissors AND humanChoice is paper) THEN
-        INCREMENT computer choice score
-        DISPLAY computer winner message
+    ELSE IF (playerScore > computerScore) THEN
+        DISPLAY player 1 win message
     ELSE 
-        DISPLAY invalid round message
+        DISPLAY computer win message
     ENDIF
     */
-   let hC = humanChoice.toLowerCase();
-   let cC = computerChoice.toLowerCase();
 
-   if (hC === cC) {
-        console.log("Tie Game!");
+    function playRound(humanChoice, computerChoice) {
+        /*
+        SET humanChoice and computerChoice variables to lower case
+        IF humanChoice is equal to computerChoice THEN
+            DISPLAY tie message
+            RETURN tie string
+        ELSE IF (humanChoice is rock AND computerChoice is scissors) OR 
+                (humanChoice is paper AND computerChoice is rock) OR 
+                (humanChoice is scissors AND computerChoice is paper) THEN
+            INCREMENT human choice score
+            DISPLAY human winner message
+            RETURN human winner string
+        ELSE IF (computerChoice is rock AND humanChoice is scissors) OR 
+                (computerChoice is paper AND humanChoice is rock) OR 
+                (computerChoice is scissors AND humanChoice is paper) THEN
+            INCREMENT computer choice score
+            DISPLAY computer winner message
+            RETURN computer winner string
+        ELSE 
+            DISPLAY invalid round message
+        ENDIF
+        */
+       let hC = humanChoice.toLowerCase();
+       let cC = computerChoice.toLowerCase();
+    
+       if (hC === cC) {
+            console.log("Tie Game!");
+            return tieStr;
+       } else if ((hC === "rock" && cC === "scissors") || 
+                  (hC === "paper" && cC === "rock") || 
+                  (hC === "scissors" && cC === "paper")) {
+            humanScore++;
+            console.log(`You win! ${hC} beats ${cC}.`);
+            return humanWinStr;
+       } else if ((cC === "rock" && hC === "scissors") || 
+                  (cC === "paper" && hC === "rock") || 
+                  (cC === "scissors" && hC === "paper")) {
+            computerScore++;
+            console.log(`You lose! ${cC} beats ${hC}.`);  
+            return computerWinStr;
+        } else {
+            console.log("Try again. You have inputted in an invalid choice.");
+       }
+    }
 
-   } else if ((hC === "rock" && cC === "scissors") || 
-              (hC === "paper" && cC === "rock") || 
-              (hC === "scissors" && cC === "paper")) {
-        humanScore++;
-        console.log(`You win! ${hC} beats ${cC}.`);
+    // Game Scores
+    let humanScore = 0;
+    let computerScore = 0;
 
-   } else if ((cC === "rock" && hC === "scissors") || 
-              (cC === "paper" && hC === "rock") || 
-              (cC === "scissors" && hC === "paper")) {
-        computerScore++;
-        console.log(`You lose! ${cC} beats ${hC}.`);   
+    // Player Selections
+    let humanSelection = null;
+    let computerSelection = null;
+
+    let numOfRounds = 5;
+
+    let winnerStr = null;
+    let humanWinStr = "HW";
+    let computerWinStr = "CW";
+    let tieStr = "T";
+
+    for (let i = 0; i < numOfRounds; i++) {
+        humanSelection = getHumanChoice();
+        computerSelection = getComputerChoice();
+
+        winnerStr = playRound(humanSelection, computerSelection);
+
+        if (winnerStr === humanWinStr) {
+            humanScore += 1;
+        } else if (winnerStr === computerWinStr) {
+            computerScore += 1;
+        } else if (winnerStr === tieStr) {
+            // Do nothing
+        } else {
+            console.log("Invalid winner")
+        }
+    }
+    
+    if (humanScore === computerScore) {
+        console.log("Tie game between 5 rounds!")
+    } else if (humanScore > computerScore) {
+        console.log("You win between the 5 rounds!")
     } else {
-        console.log("Try again. You have inputted in an invalid choice.");
-   }
+        console.log("You lose, the computer wins between the 5 rounds!")
+    }
 }
 
-const computerSelection = getComputerChoice();
-const humanSelection = getHumanChoice();
-
-console.log(humanSelection, computerSelection);
-
-playRound(humanSelection, computerSelection);
+playGame();
